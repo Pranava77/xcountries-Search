83864b7386af4b7f3dc1f5edd939a831e15d9612
Pranava Holla <85442353+Pranava77@users.noreply.github.com>
2024-11-25 17:46:10 +0530

    implemented api and search functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,55 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [search, setSearch] = useState('')
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    fetchCountries();
+  }, [])
+
+  const fetchCountries = async () => {
+    try{
+      const response = await fetch('https://restcountries.com/v3.1/all')
+      const data = await response.json()
+      setCountries(data)
+      console.log(data, 'data');  
+    } catch(error) {
+      console.log(error, 'error')
+    }
+  };
+
+  const filteredCountires = countries.filter((country) => {
+    return country.name.common.toLowerCase().includes(search.toLowerCase())
+  })
+
 
   return (
     <>
       <div>
+
         <input 
           type="text"
+          value={search}
           placeholder='Search for Countries'
           className='searchInput'
-          onChange={(e) => console.log(e.target.value)}
+          onChange={(e) => setSearch(e.target.value)}
           />
+
         <div className='grid'>
-            <div className="countryCard">
-              <img className='countryFlag' src="https://flagcdn.com/w320/ae.png" alt="" />
-              <p className="countryName">United Arab Emirates</p>
+          {filteredCountires.map((country) => (
+            <div className="countryCard" key={country.cca3}>
+              <img 
+                className='countryFlag' 
+                src={country.flags.png} 
+                alt={`Flag of ${country.name.common}`} 
+              />
+              <p className="countryName">{country.name.common}</p>
             </div>
-            <div className="countryCard">
-              <img className='countryFlag' src="https://flagcdn.com/w320/ae.png" alt="" />
-              <p className="countryName">United Arab Emirates</p>
-            </div>
-            <div className="countryCard">
-              <img className='countryFlag' src="https://flagcdn.com/w320/ae.png" alt="" />
-              <p className="countryName">United Arab Emirates</p>
-            </div>
-          </div>
+          ))}
+        </div>
       </div>
     </>
   )
