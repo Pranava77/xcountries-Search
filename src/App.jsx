67f1d4ed0fd60bb6990cbc1e6f5f3606fc51d57c
Pranava Holla <85442353+Pranava77@users.noreply.github.com>
2024-11-25 18:05:56 +0530

    added new error handlers

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,21 @@ function App() {
   const [debouncedSearch, setDebouncedSearch] = useState('')
 
   useEffect(() => {
+    const fetchCountries = async () => {
+      try {
+        const response = await fetch('https://restcountries.com/v3.1/all');
+        if (!response.ok) {
+          throw new Error('Failed to fetch countries');
+        }
+        const data = await response.json();
+        setCountries(data);
+      } catch (error) {
+        console.error('Error fetching countries:', error);
+      }
+    };
+
     fetchCountries();
-  }, [])
+  }, []);
 
 
   useEffect(() => {
@@ -21,48 +34,36 @@ function App() {
   }, [search])
 
 
-  const fetchCountries = async () => {
-    try{
-      const response = await fetch('https://restcountries.com/v3.1/all')
-      const data = await response.json()
-      setCountries(data)
-      console.log(data, 'data');  
-    } catch(error) {
-      console.log(error, 'error')
-    }
-  };
-
-  const filteredCountires = countries.filter((country) => {
+  const filteredCountries = countries.filter((country) => {
     return country.name.common.toLowerCase().includes(debouncedSearch.toLowerCase())
   })
 
 
   return (
-    <>
-      <div>
+    <div className="container">
+      <input 
+        type="text"
+        value={search}
+        placeholder='Search for Countries'
+        className='searchInput'
+        onChange={(e) => setSearch(e.target.value)}
+        data-testid="search-input"
+      />
 
-        <input 
-          type="text"
-          value={search}
-          placeholder='Search for Countries'
-          className='searchInput'
-          onChange={(e) => setSearch(e.target.value)}
-          />
-
-        <div className='grid'>
-          {filteredCountires.map((country) => (
-            <div className="countryCard" key={country.cca3}>
-              <img 
-                className='countryFlag' 
-                src={country.flags.png} 
-                alt={`Flag of ${country.name.common}`} 
-              />
-              <p className="countryName">{country.name.common}</p>
-            </div>
-          ))}
-        </div>
+      <div className='grid'>
+        {filteredCountries.map((country) => (
+          <div className="country-container" key={country.cca3} data-testid="country-container">
+            <img 
+              className='countryFlag' 
+              src={country.flags.png} 
+              alt={`Flag of ${country.name.common}`}
+              data-testid="country-flag"
+            />
+            <p className="countryName" data-testid="country-name">{country.name.common}</p>
+          </div>
+        ))}
       </div>
-    </>
+    </div>
   )
 }
 
